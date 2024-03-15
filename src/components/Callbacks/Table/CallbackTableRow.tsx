d1405ec826ac884e5dbe4b9b0adc2b4e69359d51
Diego Lafuente <dlaf88@gmail.com>
2024-03-15 13:37:26 -0400

    finished refactoring for edit mode

--- src/components/Callbacks/Table/CallbackTableRow.tsx
+++ src/components/Callbacks/Table/CallbackTableRow.tsx
@@ -1,34 +1,11 @@
-import React from "react";
-
+import React, { useContext } from "react";
 import { CallbackWithId } from "../../../types";
 import { extractKeyValues } from "../helpers/callbacksTableUtils";
 import LabelRow from "./LabelRow";
 import type { Dispatcher } from "../helpers/reducer/reducerFunction";
 import dispatchFunctionFactory from "../helpers/reducer/dispatchFunctionFactory";
-import { Form } from "react-bootstrap";
-
-type Option = {
-  text: string;
-  value: string;
-};
-
-type SelectProps = {
-  options: Option[];
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-};
-
-const Select = ({ options, value, onChange }: SelectProps) => {
-  return (
-    <Form.Control as="select" value={value} onChange={onChange}>
-      {options.map((option, index) => (
-        <option key={index} value={option.value}>
-          {option.text}
-        </option>
-      ))}
-    </Form.Control>
-  );
-};
+import { TableContext } from "./TableContext";
+import EventSelect, {Option} from "./EventSelect";
 
 const defaultOptions: Option[] = [
   { text: "After Complete", value: "after_complete" },
@@ -42,6 +19,8 @@ const CallbackTableRow = ({
   callbackWithId: CallbackWithId;
   dispatcher: Dispatcher;
 }) => {
+  const { editMode } = useContext(TableContext) as { editMode: boolean };
+
   const update = dispatchFunctionFactory(dispatcher).update;
   const returnOnChange = (key: string, value: string) => {
     return (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -56,17 +35,21 @@ const CallbackTableRow = ({
   return (
     <tr>
       <td>
-        <Select
-          options={defaultOptions}
-          value={callback.event}
-          onChange={returnOnSelect}
-        ></Select>
+        {editMode ? (
+          <EventSelect
+            options={defaultOptions}
+            value={callback.event}
+            onChange={returnOnSelect}
+          ></EventSelect>
+        ) : (
+          callback.event
+        )}
       </td>
       <td>{callback.function}</td>
       <td>
         {extractKeyValues(callback.args).map((keyValue, index) => (
           <LabelRow
-            editMode={true}
+            editMode={editMode}
             key={index}
             label={keyValue.label}
             value={keyValue.value}
