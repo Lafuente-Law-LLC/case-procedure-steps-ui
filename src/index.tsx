7638ccc2c04f2b5222e42d28c027b92182c1f4e6
Diego Lafuente <dlaf88@gmail.com>
2024-06-09 12:34:19 -0400

    refactored the index

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import CaseProcedureApp from "./CaseProcedureApp";
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./css/main.scss";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
-root.render(<CaseProcedureApp title="" description="" initialData={{}} onSubmitFunction={()=>{}} />);
-
-
+root.render(
+  <CaseProcedureApp
+    title=""
+    description=""
+    initialData={{}}
+    onSubmitFunction={() => {}}
+  />,
+);
 
 export { CaseProcedureApp };
