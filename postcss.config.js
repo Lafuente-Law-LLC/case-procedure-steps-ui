e0b73714e0a96040ea1783513bbcffc9b3686936
Diego Lafuente <dlaf88@gmail.com>
2024-06-09 16:26:05 -0400

    changes to the css

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,11 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
+//const purgecss = require("@fullhuman/postcss-purgecss");
 const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
     require("postcss-preset-env")({}),
     require("autoprefixer"),
-    purgecss({
-      content: [
-        "./src/css/main.scss",
-        "./src/**/*.html",
-        "./src/**/*.tsx",
-        "./src/**/*.js",
-      ], 
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    }),
+
     cssnano({
       preset: [
         "default",
